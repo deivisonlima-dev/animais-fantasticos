538ccc64a36b29460e2bf7cd64449bf45be568da
Deivison Lima <deivisontrovao@gmail.com>
2023-05-22 11:52:28 -0300

    scrollsuave transformado em objeto

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,20 +1,33 @@
 // Scroll suave - link interno
-const smoothScrolling = () => {
-  const internalLinks = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
-  if (internalLinks.length) {
-    /** @param {MouseEvent} event */
-    const scrollToSection = (event) => {
-      event.preventDefault();
-      const href = event.currentTarget.getAttribute('href');
-      const section = document.querySelector(href);
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-    };
-    internalLinks.forEach((link) => {
-      link.addEventListener('click', scrollToSection);
+class SmoothScrolling {
+  constructor(links, options) {
+    this.internalLinks = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection = (event) => {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  };
+
+  addLinkEvent() {
+    this.internalLinks.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
     });
   }
-};
-export default smoothScrolling;
+
+  init() {
+    if (this.internalLinks.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
+export default SmoothScrolling;
