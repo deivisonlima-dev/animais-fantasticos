e9c5d9b630e87ba868d8125688278adbac3b221a
Deivison Lima <deivisontrovao@gmail.com>
2023-05-29 10:38:25 -0300

    refatoracao com class animate-scroll

--- js/modules/animate-scroll.js
+++ js/modules/animate-scroll.js
@@ -1,20 +1,50 @@
 // Animar scroll
-const initAnimationScroll = () => {
-  const sections = document.querySelectorAll('[data-animate="scroll"]');
-  if (sections.length) {
-    const windowHalf = window.innerHeight * 0.6;
-    const animateScrolling = () => {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top - windowHalf;
-        if (sectionTop < 0) {
-          section.classList.add('animate');
-        } else if (section.classList.contains('animate')) {
-          section.classList.remove('animate');
-        }
-      });
-    };
-    animateScrolling();
-    window.addEventListener('scroll', animateScrolling);
+class AnimationScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowHalf = window.innerHeight * 0.6;
   }
-};
-export default initAnimationScroll;
+
+  animateScrolling = () => {
+    this.sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top - this.windowHalf;
+      if (sectionTop < 0) {
+        section.classList.add('animate');
+      } else if (section.classList.contains('animate')) {
+        section.classList.remove('animate');
+      }
+    });
+  };
+
+  init() {
+    this.animateScrolling();
+    window.addEventListener('scroll', this.animateScrolling);
+  }
+}
+export default AnimationScroll;
+
+// class AnimationScroll {
+//   constructor(sections) {
+//     this.sections = document.querySelectorAll(sections);
+//   }
+
+//   animateScrolling() {
+//     const myObserver = new IntersectionObserver((entries) => {
+//       entries.forEach((entry) => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add('animate');
+//         } else if (entry.target.classList.contains('animate')) {
+//           entry.target.classList.remove('animate');
+//         }
+//       });
+//     });
+//     this.sections.forEach((element) => {
+//       myObserver.observe(element);
+//     });
+//   }
+
+//   init() {
+//     this.animateScrolling();
+//   }
+// }
+// export default AnimationScroll;
